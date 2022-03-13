29f2ff2acd43169f23a15afd6916f3ac0487e867
xTidewaterx <79268288+xTidewaterx@users.noreply.github.com>
2022-03-13 23:50:12 +0100

    Add files via upload

---
+++ change3.js
@@ -0,0 +1,16 @@
+
+const showMore = document.querySelector(".aboutUsColumn");
+const showMoreButtonClass = document.querySelector(".buttonClass");
+
+
+
+
+function showMoreButton() {
+
+    showMore.style.height = "auto";
+   showMoreButtonClass.style.display = "none";
+
+
+
+}
+
